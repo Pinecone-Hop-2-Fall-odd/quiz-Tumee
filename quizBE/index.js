2be5534b9d677c1c652d5da2ed8b1135d1dff540
Түмэн-өлзий Нарандаваа <HP230887@G303-CC0138.local>
2023-12-08 19:44:46 +0800

    commit

--- quizBE/index.js
+++ quizBE/index.js
@@ -1,6 +1,5 @@
 import express, { response } from "express";
 import cors from "cors";
-import fs from "fs"
 import mongoose, { connect } from "mongoose";
 const port = 8000;
 const app = express();
@@ -10,12 +9,15 @@ import { Router } from "./src/router/quiz-router.js";
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(Router)
+app.use(Router);
 
 const connectUser = async () => {
-  await mongoose.connect('mongodb+srv://Tum:Tumee0205@cluster0.femwxfk.mongodb.net/')
-}
-connectUser()
+  await mongoose.connect(
+    "mongodb+srv://Tum:Tumee0205@cluster0.femwxfk.mongodb.net/"
+  );
+  console.log("connected");
+};
+connectUser();
 app.listen(port, () => {
   console.log("Server is running on http://localhost:" + port);
 });
--- quizBE/src/models/user-model.js
+++ quizBE/src/models/user-model.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    userName: String,
+    username: String,
     password: String,
-    email: { type: String, unique: true },
+    // email: { type: String, unique: true },
     createdOn: Date,
 });
 
